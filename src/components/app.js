99e0cf2673e697505424bef4135fa3ffa64e48d2
AnirudhChopra2810 <65032965+AnirudhChopra2810@users.noreply.github.com>
2021-11-06 21:35:53 +0530

    sidebar added

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import SidebarMenu from './sidebarMenu';
 import Nav from './nav';
 import config from '../assets/config';
 import ProviderList from './providerlist';
+
 import './styles.css';
 
 const { YT_CHANNEL_IDS, API_KEY } = config;
@@ -10,13 +10,14 @@ const { YT_CHANNEL_IDS, API_KEY } = config;
 const App = () => {
 	const [sidebar, setSidebar] = useState(false);
 
+	console.log(sidebar);
+
 	return (
 		<div>
-			<Nav setSidebar={setSidebar} />
-			<ProviderList channelId={YT_CHANNEL_IDS.NETFLIX} apiKey={API_KEY} />
+			<Nav />
+			{/* <ProviderList channelId={YT_CHANNEL_IDS.NETFLIX} apiKey={API_KEY} /> 
 			<ProviderList channelId={YT_CHANNEL_IDS.AMAZON_PRIME} apiKey={API_KEY} />
-			{/*sidebar === true && <SidebarMenu setSidebar={setSidebar} sidebar={sidebar} />*/}
-			{/* <Cards /> */}
+			<Cards /> */}
 		</div>
 	);
 };
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,22 +1,69 @@
 import { useState } from 'react';
-import { Menu, Segment, Header, Icon } from 'semantic-ui-react';
+import { Menu, Segment, Header, Icon, Grid, Sidebar, Image } from 'semantic-ui-react';
+import config from '../assets/config';
+import ProviderList from './providerlist';
+const { YT_CHANNEL_IDS, API_KEY } = config;
 
 const Nav = (props) => {
+	const [visible, setVisible] = useState(false);
+
 	const handleClick = () => {
-		props.setSidebar(true);
+		setVisible(true);
+		console.log('yo');
 	};
 	return (
-		<div style={{ backgroundColor: 'black' }}>
-			<Menu inverted size="huge">
-				<Menu.Item header>Know Your Shows</Menu.Item>
-				<Menu.Menu position="right">
-					<Menu.Item onClick={handleClick}>
-						<Icon name="th" color="yellow" />
-					</Menu.Item>
-				</Menu.Menu>
-			</Menu>
+		<div>
+			<div style={{ backgroundColor: 'black' }}>
+				<Menu inverted size="huge">
+					<Menu.Item header>Know Your Shows</Menu.Item>
+					<Menu.Menu position="right">
+						<Menu.Item onClick={handleClick}>
+							<Icon name="th" color="yellow" />
+						</Menu.Item>
+					</Menu.Menu>
+				</Menu>
+			</div>
+			<Grid columns={1}>
+				<Grid.Column>
+					<Sidebar.Pushable as={Segment}>
+						<Sidebar
+							as={Menu}
+							animation="overlay"
+							icon="labeled"
+							inverted
+							onHide={() => {
+								setVisible(false);
+							}}
+							vertical
+							visible={visible}
+							width="thin"
+						>
+							<Menu.Item as="a">
+								<Icon name="home" />
+								Home
+							</Menu.Item>
+							<Menu.Item as="a">
+								<Icon name="gamepad" />
+								Games
+							</Menu.Item>
+							<Menu.Item as="a">
+								<Icon name="camera" />
+								Channels
+							</Menu.Item>
+						</Sidebar>
+
+						<Sidebar.Pusher dimmed={visible}>
+							<Segment basic>
+								<ProviderList channelId={YT_CHANNEL_IDS.NETFLIX} apiKey={API_KEY} />
+								<ProviderList channelId={YT_CHANNEL_IDS.AMAZON_PRIME} apiKey={API_KEY} />
+							</Segment>
+						</Sidebar.Pusher>
+					</Sidebar.Pushable>
+				</Grid.Column>
+			</Grid>
 		</div>
 	);
 };
 
 export default Nav;
+// {sidebar === true && <SidebarMenu setSidebar={setSidebar} sidebar={sidebar} />}
