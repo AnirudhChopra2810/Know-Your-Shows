3f5b25c72d3b114914bb4672162b15ce7c1ebec3
AnirudhChopra2810 <65032965+AnirudhChopra2810@users.noreply.github.com>
2021-11-17 15:25:03 +0530

    one time update mechanism implemented on server side.

--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ const axios = require("axios")
 const cors = require("cors");
 app.use(cors());
 const path = require("path");
-const fs = require("fs");
-const { create } = require("domain");
 const dotenv = require("dotenv");
 dotenv.config({path: "./config.env"});
 
@@ -13,62 +11,55 @@ const bodyParser = require("body-parser");
 const { json } = require("body-parser");
 app.use(bodyParser.json());
 
-const staticPath = path.join(__dirname, "../client/build");
-app.use(express.static(__dirname));
+// const staticPath = path.join(__dirname, "../client/build");
+// app.use(express.static(staticPath));
 
 const config = process.env;
 console.log(config.API_KEY);
+    let cachedData = null;
+    let lastUpdateTime = 0;
+    const minutes = 1;
+    const seconds = 60;
+    const updateInterval = minutes*seconds*1000;
 
-
-function checkTime (req, res, next){
-    let date = new Date();
-    let currentMinutes = date.getMinutes();
-    console.log(currentMinutes);
-    
-    
-    fs.readFile('apiData.txt', 'utf-8',  (err, data) => {
-        let _data = JSON.parse(data);
-        let minutes = _data.hours;
-        if(currentMinutes >= minutes){
-          console.log("hello"); 
-          next();
-        }else res.send("wait");
-
-    })
-}
-
-function getData (){
-
-    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=Trailer&channelId=UCZSNzBgFub_WWil6TOTYwAg&key=AIzaSyB_AZeQnCsy3Lm7DklpKpzoF5WKD90vTEg`;
-
-    axios
-	.get(url)
-	.then((response) => {
-				console.log('hello there');
-                console.log(response);
-				let data = response.data.items;
-                const date = new Date();
-                const hours = date.getMinutes();
-                const collection = {link: data, hours: hours};
-                const link = JSON.stringify(collection);
-                fs.writeFile('apiData.txt', link, function(error){
-                   return console.log(error);
-                })
-				
-			})
-			.catch((error) => console.log(error));
-        }
-
-
-// app.use(checkTime);
-
-app.get("/", (req, res) => {
-    try {
-        getData();
+ const checkTime = async () => {
+    if(!cachedData || !lastUpdateTime || (lastUpdateTime + updateInterval) < Date.now()){
+        cachedData = await getData(cachedData);
+        lastUpdateTime = Date.now();
         console.log("i worked");
-        fs.readFile('apiData.txt', 'utf-8', (err, data) => {
-            return res.send(data);
-        } )
+    }
+    return cachedData;
+ }       
+
+
+const  getData = async  (cachedData) => {
+    
+    let collection;
+    maxResult = 20;
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResult}&q=Trailer&channelId=${config.NETFLIX}&key=${config.API_KEY}`;
+
+    const Data = await axios
+	.get(url)
+	.then((response) => { 
+        const data = response.data.items;
+        const millisec = Date.now();
+        const collection = {items: data, date: millisec }
+        return collection;
+    })
+    // .then((data) => {
+    //     cachedData = data.data;
+    //     return cachedData;
+    // })
+	.catch((error) => console.log(error));
+         return Data;   
+   }
+
+
+app.get("/", async (req, res) => {
+    try {
+        checkTime();
+        console.log(lastUpdateTime);
+        // console.log(cachedData);
     } catch (error) {
         console.log(error);
     }
